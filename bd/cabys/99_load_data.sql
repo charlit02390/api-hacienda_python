567b36b1c8a4289b597e9835310f295380807fa2
Luigi González <cr_luigi@hotmail.com>
2020-11-02 10:10:16 -0600

    Actualizacion de datos CABYS
    Se actualizaron los datos de las tablas de CABYS a la última
    versión del Catálogo para la fecha 15 de Octubre del 2020.

--- bd/cabys/99_load_data.sql
+++ bd/cabys/99_load_data.sql
@@ -1,3 +1,6 @@
+TRUNCATE cabysxsac;
+TRUNCATE medicamento;
+DELETE FROM cabys;
 -- Must change file path so it points to the proper location of each file. Files'll be inside "data" folder where this script is, but the full path will differ from environment to environment.
 -- E.g. C:/api/bd/cabys/data/cabys_data.txt
 LOAD DATA INFILE '/path/to/bd/cabys/data/cabys_data.txt' INTO TABLE cabys FIELDS TERMINATED BY '$' LINES TERMINATED BY '\r\n';
