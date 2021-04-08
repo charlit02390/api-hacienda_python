c1b0202156b8180f2144bdef2c6686dd61a6e25d
Luigi González <cr_luigi@hotmail.com>
2021-04-08 16:18:36 -0600

    Arreglos miscelaneos
    - Ajuste de esquema mh_validations.
    - Ajuste del asunto en los correos electronicos de documentos para
    utilizar el consecutivo en lugar de la clave e indicar el nombre del
    emisor.

--- service/emails.py
+++ service/emails.py
@@ -4,17 +4,10 @@ from infrastructure import documents
 from configuration import globalsettings
 from infrastructure.emails import send_email
 from service import fe_enums
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
-import ssl
-from infrastructure.dbadapter import DatabaseError
-import logging
 from helpers.errors.enums import InputErrorCodes
 from helpers.errors.exceptions import InputError
 from helpers.utils import build_response_data
+from helpers.validations.document import KEY_PARTS_SLICES
 
 cfg = globalsettings.cfg
 
@@ -47,7 +40,13 @@ def sent_email_fe(data):
     if isinstance(additional_recipients, list):
         receivers += list(x['email'] for x in additional_recipients)
 
-    subject = "Envio de " + fe_enums.tagNamePDF[document['document_type']] + ' Numero: ' + document['key_mh']
+    sequence_slice = KEY_PARTS_SLICES['sequence']
+    subject = "Envio de {}: {} del Emisor: {}".format(
+        fe_enums.tagNamePDF[document['document_type']],
+        document['key_mh'][sequence_slice],
+        document['name']
+    )
+
     body = 'Adjuntamos los datos de la ' + fe_enums.tagNamePDF[document['document_type']]
 
     name_file1 = fe_enums.tagNamePDF[document['document_type']] + '_' + document['key_mh'] + '.pdf'
@@ -107,55 +106,3 @@ def send_custom_email(data, file1, file2, file3):
     send_email(receivers, host, sender, port, encrypt_type,
                username, password, subject, content, attachments)
     return build_response_data({'message': 'email sent successfully'})
-
-
-def sent_email(pdf, signxml):  # not used. Prolly a prototype to infrastructure.email.send_email
-
-    subject = "An email with attachment from Python"
-    body = "This is an email with attachment sent from Python"
-    sender_email = cfg['email']['user']
-    receiver_email = "charlit02390@gmail.com"
-    password = cfg['email']['password']
-    # Create a multipart message and set headers
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = subject
-    message["Bcc"] = receiver_email  # Recommended for mass emails
-    # Add body to email
-    message.attach(MIMEText(body, "plain"))
-    filename = "document.pdf"  # In same directory as script
-    # Open PDF file in binary mode
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(pdf)
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= example.pdf"
-    )
-
-    part2 = MIMEBase("application", "octet-stream")
-    part2.set_payload(signxml)
-
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part2)
-
-    # Add header as key/value pair to attachment part
-    part2.add_header(
-        "Content-Disposition",
-        f"attachment; filename= FE_comprobante.xml",
-    )
-
-    # Add attachment to message and convert message to string
-    message.attach(part)
-    message.attach(part2)
-    text = message.as_string()
-
-    # Log in to server using secure context and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP(cfg['email']['host'], cfg['email']['port']) as server:
-        server.starttls(context=context)
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, text)
